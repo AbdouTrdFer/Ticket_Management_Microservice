eac0c755cd32593c105fb704c880075dec670102
AbdouTrdFer <ferhanabdelali@gmail.com>
2025-11-09 21:33:02 +0100

    update endpoints

---
+++ src/main/java/ticketmanagement/ticket_management_microservice/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package ticketmanagement.ticket_management_microservice.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // 🔒 Nouvelle syntaxe (Spring Security 6.1+)
+        http
+                .securityMatcher("/**")
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(form -> form.disable());
+
+        return http.build();
+    }
+}
+
